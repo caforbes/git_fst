a2acfe33a97e2d883d5233f3fb3a069d05679d3d
Clarissa Forbes <for.c.forbes@gmail.com>
2021-09-20 11:24:31 -0700

    rules: big T elsewhere form (stem-final) is (t)

--- test/test_output_actual_words.py
+++ test/test_output_actual_words.py
@@ -40,10 +40,15 @@ class TestLexiconImport(TestFSTOutput):
         analyzed = self.fst.analyze(lower)
         self.assertEqual(len(analyzed), 1)
     
-    def test_bats_bigT(self):
+    def test_bats_bigT_bare(self):
         root = "b$atsT"
-        self.assertNotEqual(len(self.fst.generate(root+'+VT')), 0)
+        bare_result = self.fst.generate(root+'+VT')
 
+        self.assertNotEqual(len(bare_result), 0)
+        self.assertIn("bats(t)", bare_result)
+    
+    def test_bats_bigT_paths(self):
+        root = "b$atsT"
         # path should exist
         paths = [
             '+VT-3.II',
@@ -52,7 +57,6 @@ class TestLexiconImport(TestFSTOutput):
         for path in paths:
             with self.subTest(path=path):
                 self.assertNotEqual(len(self.fst.generate(root+path)), 0)
-        
         # path should not exist
         paths = [
             '+VT-T-3.II',
@@ -62,10 +66,16 @@ class TestLexiconImport(TestFSTOutput):
             with self.subTest(path='no '+path):
                 self.assertEqual(len(self.fst.generate(root+path)), 0)
 
-    def test_tahl_bigT(self):
+    def test_tahl_PLbigT_bare(self):
         root = "t'$ahlT"
-        self.assertNotEqual(len(self.fst.generate(root+'+VT')), 0)
+        bare_result = self.fst.generate(root+'+VT')
 
+        self.assertNotEqual(len(bare_result), 0)
+        self.assertIn("t'ahl(t)", bare_result)
+
+    def test_tahl_PLbigT_paths(self):
+        root = "t'$ahlT"
+        # path should exist
         paths = [
             '+VT-3.II',
             '+VT-TR-3.II',
@@ -73,7 +83,6 @@ class TestLexiconImport(TestFSTOutput):
         for path in paths:
             with self.subTest(path=path):
                 self.assertNotEqual(len(self.fst.generate(root+path)), 0)
-
         # path should not exist
         paths = [
             '+VT-T-3.II',
